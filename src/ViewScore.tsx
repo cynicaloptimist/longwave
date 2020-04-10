2f52411a546ed517630e20dc5ee30a771a129e7a
Evan Bailey <baileyevang@gmail.com>
2020-04-10 16:19:47 -0700

    Show both team scores after guess

--- src/ViewScore.tsx
+++ src/ViewScore.tsx
@@ -1,18 +1,27 @@
 import React from "react";
 import { getScore } from "./getScore";
+import { Row } from "./LayoutElements";
 
 export function ViewScore(props: {
   spectrumTarget: number;
   guess: number;
+  leftScore: number;
+  rightScore: number;
   nextRound: () => void;
 }) {
   const score = getScore(props.spectrumTarget, props.guess);
-  return (<div>
-    <div>Target: {props.spectrumTarget}</div>
-    <div>Guess: {props.guess}</div>
-    <div>Score: {score} points!</div>
+  return (
     <div>
-      <input type="button" value="Next Round" onClick={props.nextRound} />
+      <div>Target: {props.spectrumTarget}</div>
+      <div>Guess: {props.guess}</div>
+      <div>Score: {score} points!</div>
+      <Row>
+        <div>Left Brain: {props.leftScore}</div>
+        <div>Right Brain: {props.rightScore}</div>
+      </Row>
+      <div>
+        <input type="button" value="Next Round" onClick={props.nextRound} />
+      </div>
     </div>
-  </div>);
+  );
 }
