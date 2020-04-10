03620e948de743d5b94abfbc9b1056c23cc90cab
Evan Bailey <baileyevang@gmail.com>
2020-04-09 17:52:36 -0700

    roundtrip to realtimeDB

--- src/GameRoom.tsx
+++ src/GameRoom.tsx
@@ -1,7 +1,54 @@
 import { useParams } from "react-router-dom";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { database } from "firebase";
+
+interface GameState {
+  roomId: string;
+  increment: number;
+}
 
 export function GameRoom() {
   const { roomId } = useParams();
-  return <div>{roomId || "Room Id missing"}</div>;
+  if (roomId === undefined) {
+    throw new Error("RoomId missing");
+  }
+
+  const [gameState, setGameState] = useState<GameState>({
+    roomId,
+    increment: 0,
+  });
+
+  useEffect(() => {
+    const dbRef = database().ref("rooms/" + roomId);
+    dbRef.on("value", (appState) => {
+      if (!appState.val()) {
+        return;
+      }
+      setGameState(appState.val());
+    });
+    return () => dbRef.off();
+  }, [roomId]);
+
+  return (
+    <div>
+      {gameState.roomId || "Room Id missing"}
+      <div
+        onClick={() =>
+          writeGameState({
+            roomId,
+            increment: gameState.increment + 1,
+          })
+        }
+      >
+        Increment:{" "}
+      </div>
+      {gameState.increment}
+    </div>
+  );
+}
+
+function writeGameState(gameState: GameState) {
+  database()
+    .ref("rooms/" + gameState.roomId)
+    .set(gameState);
 }
