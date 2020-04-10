941617a1fb0e3603c82305059ed580621ff05f41
Evan Bailey <baileyevang@gmail.com>
2020-04-09 17:34:21 -0700

    use imported firebase instead of SDK urls

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAyPMuIGreq81gLErYeKGkEje851Xdh_kc",
+  authDomain: "wavelength-online.firebaseapp.com",
+  databaseURL: "https://wavelength-online.firebaseio.com",
+  projectId: "wavelength-online",
+  storageBucket: "wavelength-online.appspot.com",
+  messagingSenderId: "811157116454",
+  appId: "1:811157116454:web:be3d3e6b17629700c038e7",
+  measurementId: "G-XZ13S15LMC"
+};
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { initializeApp } from 'firebase';
+import { firebaseConfig } from './firebaseConfig';
+
+initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
