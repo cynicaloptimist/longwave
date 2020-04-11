953a72c03b7dc2ba6a2d55d0387a24fbd0fad5d6
Evan Bailey <baileyevang@gmail.com>
2020-04-10 19:53:15 -0700

    formatting

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { initializeApp } from 'firebase';
-import { firebaseConfig } from './firebaseConfig';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { initializeApp } from "firebase";
+import { firebaseConfig } from "./firebaseConfig";
 
 initializeApp(firebaseConfig);
 
@@ -12,7 +13,7 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
