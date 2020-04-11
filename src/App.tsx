c8fc1287bb3a6f795aef27dfbce6d016a1310edb
Evan Bailey <baileyevang@gmail.com>
2020-04-11 15:17:26 -0700

    apply consistent margin

--- src/App.tsx
+++ src/App.tsx
@@ -8,45 +8,46 @@ import { Button } from "./Button";
 
 const style: React.CSSProperties = {
   maxWidth: 500,
-  height: "100%",
-  margin: "4px auto",
+  margin: 4,
   padding: 4,
   border: "1px solid black",
 };
 
 function App() {
   return (
-    <div style={style}>
-      <BrowserRouter>
-        <Switch>
-          <Route path="/:roomId">
-            <GameRoom />
-          </Route>
-          <Route path="/">
-            <LandingPage />
-          </Route>
-        </Switch>
-        <div
-          style={{
-            margin: 8,
-            paddingTop: 8,
-            borderTop: "1px solid black",
-            color: "gray",
-            fontSize: "small",
-          }}
-        >
-          <a
-            href="https://www.wavelength.zone/"
-            target="_blank"
-            rel="noopener noreferrer"
+    <CenteredColumn>
+      <div style={style}>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/:roomId">
+              <GameRoom />
+            </Route>
+            <Route path="/">
+              <LandingPage />
+            </Route>
+          </Switch>
+          <div
+            style={{
+              margin: 8,
+              paddingTop: 8,
+              borderTop: "1px solid black",
+              color: "gray",
+              fontSize: "small",
+            }}
           >
-            Wavelength
-          </a>{" "}
-          is designed by Wolfgang Warsch, Alex Hague, and Justin Vickers.
-          Adapted for web by Evan Bailey and Margarethe Schoen.
-        </div>
-      </BrowserRouter>
-    </div>
+            <a
+              href="https://www.wavelength.zone/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Wavelength
+            </a>{" "}
+            is designed by Wolfgang Warsch, Alex Hague, and Justin Vickers.
+            Adapted for web by Evan Bailey and Margarethe Schoen.
+          </div>
+        </BrowserRouter>
+      </div>
+    </CenteredColumn>
   );
 }
 
