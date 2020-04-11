0ec2b4ec4d4e340f1f868a348d95bf4f81000345
Evan Bailey <baileyevang@gmail.com>
2020-04-10 22:28:56 -0700

    Credit original designers

--- src/App.tsx
+++ src/App.tsx
@@ -26,6 +26,21 @@ function App() {
             <LandingPage />
           </Route>
         </Switch>
+        <div
+          style={{
+            margin: 8,
+            paddingTop: 8,
+            borderTop: "1px solid black",
+            color: "gray",
+            fontSize: "small"
+          }}
+        >
+          <a href="https://www.wavelength.zone/" target="_blank">
+            Wavelength
+          </a>{" "}
+          is designed by Wolfgang Warsch, Alex Hague, and Justin Vickers.
+          Adapted for web by Evan Bailey and Margarethe Schoen.
+        </div>
       </BrowserRouter>
     </div>
   );
