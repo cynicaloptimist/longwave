c7663944c329e4c8b0951dcb6487e6ecdbe0ae52
Evan Bailey <baileyevang@gmail.com>
2020-04-11 23:45:51 -0700

    extract component

--- src/App.tsx
+++ src/App.tsx
@@ -26,28 +26,34 @@ function App() {
               <LandingPage />
             </Route>
           </Switch>
-          <div
-            style={{
-              margin: 8,
-              paddingTop: 8,
-              borderTop: "1px solid black",
-              color: "gray",
-              fontSize: "small",
-            }}
-          >
-            <a
-              href="https://www.wavelength.zone/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Wavelength
-            </a>{" "}
-            is designed by Wolfgang Warsch, Alex Hague, and Justin Vickers.
-            Adapted for web by Evan Bailey and Margarethe Schoen.
-          </div>
+          <CommonFooter />
         </BrowserRouter>
       </div>
     </CenteredColumn>
+  );
+}
+
+function CommonFooter() {
+  return (
+    <div
+      style={{
+        margin: 8,
+        paddingTop: 8,
+        borderTop: "1px solid black",
+        color: "gray",
+        fontSize: "small",
+      }}
+    >
+      <a
+        href="https://www.wavelength.zone/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Wavelength
+      </a>{" "}
+      is designed by Wolfgang Warsch, Alex Hague, and Justin Vickers. Adapted
+      for web by Evan Bailey and Margarethe Schoen.
+    </div>
   );
 }
 
