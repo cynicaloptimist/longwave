0b03443743a6a5232740319c2e0109bca4f79016
Evan Bailey <baileyevang@gmail.com>
2020-04-13 22:06:41 -0700

    useContext in test

--- src/components/ViewScore.test.tsx
+++ src/components/ViewScore.test.tsx
@@ -1,32 +1,35 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import { ViewScore } from "./ViewScore";
-import { InitialGameState, Team } from "../state/AppState";
+import { InitialGameState, Team, BuildGameModel } from "../state/AppState";
+import { GameModelContext } from "../state/GameModelContext";
 
 test("Applies catchup rule", () => {
+  const gameState = {
+    ...InitialGameState(),
+    players: {
+      playerId: {
+        name: "Player",
+        team: Team.Left,
+      },
+      teammateId: {
+        name: "Teammate",
+        team: Team.Left,
+      },
+    },
+    leftScore: 0,
+    rightScore: 4,
+    clueGiver: "teammateId",
+    spectrumTarget: 1,
+    guess: 1,
+  };
+
   const component = render(
-    <ViewScore
-      gameState={{
-        ...InitialGameState(),
-        players: {
-          playerId: {
-            name: "Player",
-            team: Team.Left,
-          },
-          teammateId: {
-            name: "Teammate",
-            team: Team.Left,
-          },
-        },
-        leftScore: 0,
-        rightScore: 4,
-        clueGiver: "teammateId",
-        spectrumTarget: 1,
-        guess: 1,
-      }}
-      playerId={"playerId"}
-      nextRound={jest.fn()}
-    />
+    <GameModelContext.Provider
+      value={BuildGameModel(gameState, jest.fn(), "playerId")}
+    >
+      <ViewScore />
+    </GameModelContext.Provider>
   );
 
   const bonusTurn = component.getByText(
