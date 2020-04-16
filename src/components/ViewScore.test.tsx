3fe657c3892bb435cccbdd0241d811ea67df77f9
Evan Bailey <baileyevang@gmail.com>
2020-04-15 23:17:08 -0700

    Add tests around scoring boundaries

--- src/components/ViewScore.test.tsx
+++ src/components/ViewScore.test.tsx
@@ -5,16 +5,20 @@ import { InitialGameState, Team } from "../state/AppState";
 import { BuildGameModel } from "../state/BuildGameModel";
 import { GameModelContext } from "../state/GameModelContext";
 
+const onePlayerGame = {
+  ...InitialGameState(),
+  players: {
+    playerId: {
+      name: "Player",
+      team: Team.Left,
+    },
+  },
+  clueGiver: "playerId",
+};
+
 test("Applies 4 points for a perfect guess", () => {
   const gameState = {
-    ...InitialGameState(),
-    players: {
-      playerId: {
-        name: "Player",
-        team: Team.Left,
-      },
-    },
-    clueGiver: "playerId",
+    ...onePlayerGame,
     spectrumTarget: 1,
     guess: 1,
   };
@@ -28,6 +32,44 @@ test("Applies 4 points for a perfect guess", () => {
   );
 
   const bonusTurn = component.getByText("Score: 4 points!");
+  expect(bonusTurn).toBeInTheDocument();
+});
+
+test("Applies 2 points for off by 2", () => {
+  const gameState = {
+    ...onePlayerGame,
+    spectrumTarget: 1,
+    guess: 3,
+  };
+
+  const component = render(
+    <GameModelContext.Provider
+      value={BuildGameModel(gameState, jest.fn(), "playerId")}
+    >
+      <ViewScore />
+    </GameModelContext.Provider>
+  );
+
+  const bonusTurn = component.getByText("Score: 2 points!");
+  expect(bonusTurn).toBeInTheDocument();
+});
+
+test("Applies 0 points for off by 3", () => {
+  const gameState = {
+    ...onePlayerGame,
+    spectrumTarget: 1,
+    guess: 4,
+  };
+
+  const component = render(
+    <GameModelContext.Provider
+      value={BuildGameModel(gameState, jest.fn(), "playerId")}
+    >
+      <ViewScore />
+    </GameModelContext.Provider>
+  );
+
+  const bonusTurn = component.getByText("Score: 0 points!");
   expect(bonusTurn).toBeInTheDocument();
 });
 
