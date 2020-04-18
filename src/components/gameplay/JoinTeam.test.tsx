ac0528646d7dab192f436edfe6d9c693458f2082
Evan Bailey <baileyevang@gmail.com>
2020-04-17 21:54:20 -0700

    Test team listings

--- src/components/gameplay/JoinTeam.test.tsx
+++ src/components/gameplay/JoinTeam.test.tsx
@@ -1,9 +1,11 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import { render, fireEvent, within } from "@testing-library/react";
 import { GameModelContext } from "../../state/GameModelContext";
 import { BuildGameModel } from "../../state/BuildGameModel";
 import { InitialGameState, GameState, Team } from "../../state/GameState";
 import { JoinTeam } from "./JoinTeam";
+
+jest.useFakeTimers();
 
 test("Assigns player to the selected team", () => {
   const gameState: GameState = {
@@ -40,4 +42,48 @@ test("Assigns player to the selected team", () => {
       },
     },
   });
+});
+
+test("Shows current team members", () => {
+  const gameState: GameState = {
+    ...InitialGameState(),
+    players: {
+      playerId: {
+        name: "Player",
+        team: Team.Unset,
+      },
+      leftTeam1: {
+        name: "Left Team 1",
+        team: Team.Left,
+      },
+      leftTeam2: {
+        name: "Left Team 2",
+        team: Team.Left,
+      },
+      rightTeam1: {
+        name: "Right Team 1",
+        team: Team.Right,
+      },
+      rightTeam2: {
+        name: "Right Team 2",
+        team: Team.Right,
+      },
+    },
+  };
+
+  const component = render(
+    <GameModelContext.Provider
+      value={BuildGameModel(gameState, jest.fn(), "playerId")}
+    >
+      <JoinTeam />
+    </GameModelContext.Provider>
+  );
+
+  const leftBrain = within(component.getByText("LEFT BRAIN").parentElement!);
+  expect(leftBrain.getByText("Left Team 1")).toBeInTheDocument();
+  expect(leftBrain.getByText("Left Team 2")).toBeInTheDocument();
+
+  const rightBrain = within(component.getByText("RIGHT BRAIN").parentElement!);
+  expect(rightBrain.getByText("Right Team 1")).toBeInTheDocument();
+  expect(rightBrain.getByText("Right Team 2")).toBeInTheDocument();
 });
