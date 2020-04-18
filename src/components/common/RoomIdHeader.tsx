7a459e745668a231e687a71c19fc1ed3d90403f2
Evan Bailey <baileyevang@gmail.com>
2020-04-17 22:47:16 -0700

    Add menu with Reset button

--- src/components/common/RoomIdHeader.tsx
+++ src/components/common/RoomIdHeader.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { CenteredRow } from "./LayoutElements";
 import { faEllipsisV } from "@fortawesome/free-solid-svg-icons";
-import { IconButton } from "./IconButton";
+import Tippy from "@tippyjs/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useContext } from "react";
+import { GameModelContext } from "../../state/GameModelContext";
+import { InitialGameState } from "../../state/GameState";
 
 export function RoomIdHeader() {
   const { roomId } = useParams();
@@ -16,7 +20,17 @@ export function RoomIdHeader() {
       }}
     >
       <div style={{ margin: 4, padding: 4 }}>Room ID: {roomId}</div>
-      <IconButton icon={faEllipsisV} onClick={() => alert("")} />
+      <Tippy content={<RoomMenu />} interactive placement="bottom-end">
+        <div style={{ padding: 8 }}>
+          <FontAwesomeIcon icon={faEllipsisV} />
+        </div>
+      </Tippy>
     </CenteredRow>
   );
+}
+
+function RoomMenu() {
+  const { setGameState } = useContext(GameModelContext);
+
+  return <div onClick={() => setGameState(InitialGameState())}>Reset Room</div>;
 }
