b598aa3ae6f0f665d30d59d1d54d21c87fea58af
Evan Bailey <baileyevang@gmail.com>
2020-04-18 16:59:31 -0700

    test missing helptext

--- src/components/gameplay/MakeGuess.test.tsx
+++ src/components/gameplay/MakeGuess.test.tsx
@@ -13,7 +13,8 @@ test("Should show help text when more players are needed", () => {
       player1: {
         name: "Player 1",
         team: Team.Left,
-      }    },
+      },
+    },
     clueGiver: "player1",
   };
 
@@ -24,10 +25,38 @@ test("Should show help text when more players are needed", () => {
       <MakeGuess />
     </GameModelContext.Provider>
   );
-  
+
   const subject = component.queryByText(helpText);
   expect(subject).toBeInTheDocument();
-})
+});
+
+test("Should show help text when more players are needed", () => {
+  const gameState: GameState = {
+    ...InitialGameState(),
+    players: {
+      player1: {
+        name: "Player 1",
+        team: Team.Left,
+      },
+      player2: {
+        name: "Player 2",
+        team: Team.Left,
+      },
+    },
+    clueGiver: "player1",
+  };
+
+  const component = render(
+    <GameModelContext.Provider
+      value={BuildGameModel(gameState, jest.fn(), "player1")}
+    >
+      <MakeGuess />
+    </GameModelContext.Provider>
+  );
+
+  const subject = component.queryByText(helpText);
+  expect(subject).not.toBeInTheDocument();
+});
 
 test("Should show button to submit your team's guess", () => {
   const gameState: GameState = {
