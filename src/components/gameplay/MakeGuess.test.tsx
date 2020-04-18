cda998c8fd9d66c40563feb6cea06bd1e0290a99
Evan Bailey <baileyevang@gmail.com>
2020-04-18 16:58:48 -0700

    test insufficient player count help text

--- src/components/gameplay/MakeGuess.test.tsx
+++ src/components/gameplay/MakeGuess.test.tsx
@@ -5,6 +5,30 @@ import { MakeGuess } from "./MakeGuess";
 import React from "react";
 import { render } from "@testing-library/react";
 
+const helpText = "Invite other players to join the game.";
+test("Should show help text when more players are needed", () => {
+  const gameState: GameState = {
+    ...InitialGameState(),
+    players: {
+      player1: {
+        name: "Player 1",
+        team: Team.Left,
+      }    },
+    clueGiver: "player1",
+  };
+
+  const component = render(
+    <GameModelContext.Provider
+      value={BuildGameModel(gameState, jest.fn(), "player1")}
+    >
+      <MakeGuess />
+    </GameModelContext.Provider>
+  );
+  
+  const subject = component.queryByText(helpText);
+  expect(subject).toBeInTheDocument();
+})
+
 test("Should show button to submit your team's guess", () => {
   const gameState: GameState = {
     ...InitialGameState(),
