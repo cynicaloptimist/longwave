cacdc73cb582b0c0a52283f1afa64ea2d26dfe54
Evan Bailey <baileyevang@gmail.com>
2020-04-18 22:45:40 -0700

    fix layout and gameState

--- src/components/gameplay/FakeRooms.tsx
+++ src/components/gameplay/FakeRooms.tsx
@@ -1,46 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import { GameModelContext } from "../../state/GameModelContext";
 import { ActiveGame } from "./ActiveGame";
 import { BuildGameModel } from "../../state/BuildGameModel";
 import { CenteredRow, CenteredColumn } from "../common/LayoutElements";
-import { InitialGameState, GameState, Team } from "../../state/GameState";
+import {
+  InitialGameState,
+  GameState,
+  Team,
+  GameType,
+  RoundPhase,
+} from "../../state/GameState";
 import { useStorageBackedState } from "../hooks/useStorageBackedState";
 
 export function FakeRooms() {
-  const [gameState, setGameState] = useStorageBackedState<GameState>(
-    {
-      ...InitialGameState(),
-      players: {
-        ul: {
-          name: "Upper Left",
-          team: Team.Left,
-        },
-        ll: {
-          name: "Lower Left",
-          team: Team.Left,
-        },
-        ur: {
-          name: "Upper Right",
-          team: Team.Right,
-        },
-        lr: {
-          name: "Lower Right",
-          team: Team.Right,
-        },
+  const [gameState, setGameState] = useState<GameState>({
+    ...InitialGameState(),
+    gameType: GameType.Teams,
+    roundPhase: RoundPhase.PickTeams,
+    players: {
+      ul: {
+        name: "Upper Left",
+        team: Team.Left,
+      },
+      ll: {
+        name: "Lower Left",
+        team: Team.Left,
+      },
+      ur: {
+        name: "Upper Right",
+        team: Team.Right,
+      },
+      lr: {
+        name: "Lower Right",
+        team: Team.Right,
       },
     },
-    "fakeGame"
-  );
+  });
 
   const setPartialGameState = (newState: Partial<GameState>) =>
     setGameState({
-      ...InitialGameState(),
+      ...gameState,
       ...newState,
     });
 
+  const style: React.CSSProperties = {
+    width: 500,
+    margin: 4,
+    padding: 4,
+    border: "1px solid black",
+    alignItems: "stretch",
+  };
+
   return (
     <CenteredRow>
-      <CenteredColumn>
+      <CenteredColumn style={style}>
         <GameModelContext.Provider
           value={BuildGameModel(gameState, setPartialGameState, "ul")}
         >
@@ -52,7 +65,7 @@ export function FakeRooms() {
           <ActiveGame />
         </GameModelContext.Provider>
       </CenteredColumn>
-      <CenteredColumn>
+      <CenteredColumn style={style}>
         <GameModelContext.Provider
           value={BuildGameModel(gameState, setPartialGameState, "ur")}
         >
