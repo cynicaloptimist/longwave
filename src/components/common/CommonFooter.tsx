24387e06affc23c5ccd9783ec7b211b0cee0ccc9
Evan Bailey <cynicaloptimist@gmail.com>
2020-11-04 22:41:37 -0800

    Add patreon link

--- src/components/common/CommonFooter.tsx
+++ src/components/common/CommonFooter.tsx
@@ -4,20 +4,32 @@ export function CommonFooter() {
   return (
     <div
       style={{
-        margin: 8,
         paddingTop: 8,
         borderTop: "1px solid black",
         color: "gray",
         fontSize: "small",
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
       }}
     >
-      <Link href="https://www.wavelength.zone/" text="Wavelength" /> is designed
-      by Wolfgang Warsch, Alex Hague, and Justin Vickers.{" "}
-      <Link
-        href="https://github.com/cynicaloptimist/longwave"
-        text="Adapted for web"
-      />{" "}
-      by Evan Bailey and Margarethe Schoen.
+      <p style={{ margin: 8 }}>
+        <Link href="https://www.wavelength.zone/" text="Wavelength" /> is
+        designed by Wolfgang Warsch, Alex Hague, and Justin Vickers.{" "}
+        <Link
+          href="https://github.com/cynicaloptimist/longwave"
+          text="Adapted for web"
+        />{" "}
+        by Evan Bailey and Margarethe Schoen.
+      </p>
+      <a target="_blank" href="https://www.patreon.com/improvedinitiative">
+        <img
+          alt="Patreon logo"
+          title="Support us on Patreon!"
+          src="./Digital-Patreon-Wordmark_FieryCoral.png"
+          style={{ width: "150px", margin: 8 }}
+        />
+      </a>
     </div>
   );
 }
