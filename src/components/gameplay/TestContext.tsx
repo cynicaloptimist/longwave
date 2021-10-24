49c2886603afafdefc90891246395254c860c85f
Evan Bailey <cynicaloptimist@gmail.com>
2021-10-23 22:14:07 -0700

    Add Suspense and i18next to TestContext

--- src/components/gameplay/TestContext.tsx
+++ src/components/gameplay/TestContext.tsx
@@ -1,7 +1,9 @@
-import { ReactChild } from "react";
+import { ReactChild, Suspense } from "react";
 import { GameState } from "../../state/GameState";
 import { BuildGameModel } from "../../state/BuildGameModel";
 import { GameModelContext } from "../../state/GameModelContext";
+import { I18nextProvider } from "react-i18next";
+import i18n from "./i18nForTests";
 
 export function TestContext(props: {
   gameState: GameState;
@@ -15,7 +17,9 @@ export function TestContext(props: {
         "right",
       ])}
     >
-      {props.children}
+      <Suspense fallback={<div>Loading...</div>}>
+        <I18nextProvider i18n={i18n}>{props.children}</I18nextProvider>
+      </Suspense>
     </GameModelContext.Provider>
   );
 }
