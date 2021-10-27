8d671407021d881d604f6b2350084edf436a60e9
Evan Bailey <cynicaloptimist@gmail.com>
2021-10-26 22:00:55 -0700

    Switch to language dropdown

--- src/components/common/LandingPage.tsx
+++ src/components/common/LandingPage.tsx
@@ -6,42 +6,74 @@ import { LongwaveAppTitle } from "./Title";
 
 import { useTranslation } from "react-i18next";
 import { allLanguages } from "../../i18n";
+import { faLanguage } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Tooltip } from "react-tippy";
 
 export function LandingPage() {
-  const { t, i18n } = useTranslation();
-
-  const changeLanguage = (language: string | undefined) => {
-    i18n.changeLanguage(language);
-  };
+  const { t } = useTranslation();
 
   const history = useHistory();
   return (
     <CenteredColumn>
       <LongwaveAppTitle />
-      <Button
-        text={t("landingpage.create_room")}
-        onClick={() => {
-          history.push("/" + RandomFourCharacterString());
-        }}
-      />
+      <CenteredRow>
+        <Button
+          text={t("landingpage.create_room")}
+          onClick={() => {
+            history.push("/" + RandomFourCharacterString());
+          }}
+        />
+        <LanguageMenu />
+      </CenteredRow>
       <p style={{ margin: 8 }}>
         <strong>{t("landingpage.longwave")}</strong>{" "}
         {t("landingpage.adaptation")} <em>{t("landingpage.wavelength")}</em>.{" "}
         {t("landingpage.best_enjoyed")}
       </p>
-      <CenteredRow style={{ alignSelf: "flex-end" }}>
-        {allLanguages.map((language) => {
-          return (
-            <Button
-              key={language}
-              onClick={() => {
-                changeLanguage(language);
-              }}
-              text={language}
-            />
-          );
-        })}
-      </CenteredRow>
+    </CenteredColumn>
+  );
+}
+
+function LanguageMenu() {
+  return (
+    <Tooltip
+      interactive
+      position="bottom"
+      sticky
+      tabIndex={0}
+      html={<Languages />}
+    >
+      <FontAwesomeIcon size="lg" icon={faLanguage} />
+    </Tooltip>
+  );
+}
+
+function Languages() {
+  const { i18n } = useTranslation();
+
+  return (
+    <CenteredColumn
+      style={{
+        backgroundColor: "white",
+        border: "1px solid black",
+        padding: 6,
+      }}
+    >
+      {allLanguages.map((language) => {
+        return (
+          <div
+            key={language}
+            style={{ cursor: "pointer" }}
+            tabIndex={0}
+            onClick={() => {
+              i18n.changeLanguage(language);
+            }}
+          >
+            {language}
+          </div>
+        );
+      })}
     </CenteredColumn>
   );
 }
