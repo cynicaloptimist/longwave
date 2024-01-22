7de65a20ca92fd6569f94b053994351b6dd40d45
N. Westerlaken <git@nwesterlaken.nl>
2024-01-22 13:49:23 +0100

    Update i18n.tsx

--- src/i18n.tsx
+++ src/i18n.tsx
@@ -3,7 +3,7 @@ import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 
-export const allLanguages = ["en", "de", "fr", "pt-BR", "it", "nl"];
+export const allLanguages = ["en", "de", "fr", "pt-BR", "it", "nl", "nl-AVG"];
 
 i18n
   // Enables the i18next backend
