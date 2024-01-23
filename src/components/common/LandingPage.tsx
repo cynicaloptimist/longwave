f9bafa912ccb12127e04e3669742bb0f7ee04489
N. Westerlaken <git@nwesterlaken.nl>
2024-01-23 21:50:13 +0100

    Update LandingPage.tsx

--- src/components/common/LandingPage.tsx
+++ src/components/common/LandingPage.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useHistory } from "react-router-dom";
 import { RandomFourCharacterString } from "../../state/RandomFourCharacterString";
 import { CenteredColumn, CenteredRow } from "./LayoutElements";
